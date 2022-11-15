f31627afa4eee9cca8b353665301888e067dbad3
Muhammad Khan <117037989+MuhammadxKhan@users.noreply.github.com>
2022-11-15 17:09:17 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+function openMenu() {
+    document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu--open')
+}
+
+
+function openMenu2() {
+    document.body.classList += " menu--open--norm"
+}
+
+function closeMenu2() {
+    document.body.classList.remove('menu--open--norm')
+}
+
+
+
+
+/*
+
+LANDING PRODUCTS TURN WHEEL
+
+*/
+
+function wheelRight() {
+    document.body.classList += " wheel--right"
+}
+
+function wheelLeft() {
+    document.body.classList += " wheel--left"
+}
